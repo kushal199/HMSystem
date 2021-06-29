2c112d7af10c16eadc66ff4d601049e95050581d
Debajyoti Dutta <debajyotidutta.50@gmail.com>
2021-06-29 12:13:35 +0530

    cross origin
    set

--- hospitalmsystemangular/src/main/java/com/hms/HmsApplication.java
+++ hospitalmsystemangular/src/main/java/com/hms/HmsApplication.java
@@ -1,8 +1,10 @@
 package com.hms;
 import org.springframework.boot.SpringApplication;
 import org.springframework.boot.autoconfigure.SpringBootApplication;
+import org.springframework.web.bind.annotation.CrossOrigin;
 
 @SpringBootApplication
+@CrossOrigin(origins = "http://localhost:4200")
 public class HmsApplication {
     
 	public static void main(String[] args) {
